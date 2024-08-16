91feff54ea454bb0cb076a3b86e9d3b29ca73514
Kirill Mokevnin <mokevnin@gmail.com>
2024-08-16 17:04:40 -0400

    init
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- test/routes/root.test.js
+++ test/routes/root.test.js
@@ -1,8 +1,8 @@
-'use strict'
+// @ts-check
 
-const { test } = require('node:test')
-const assert = require('node:assert')
-const { build } = require('../helper')
+import { test } from 'node:test'
+import * as assert from 'node:assert'
+import { build } from '../helper.js'
 
 test('default root route', async (t) => {
   const app = await build(t)
@@ -12,17 +12,3 @@ test('default root route', async (t) => {
   })
   assert.deepStrictEqual(JSON.parse(res.payload), { root: true })
 })
-
-// inject callback style:
-//
-// test('default root route', (t) => {
-//   t.plan(2)
-//   const app = await build(t)
-//
-//   app.inject({
-//     url: '/'
-//   }, (err, res) => {
-//     t.error(err)
-//     assert.deepStrictEqual(JSON.parse(res.payload), { root: true })
-//   })
-// })
